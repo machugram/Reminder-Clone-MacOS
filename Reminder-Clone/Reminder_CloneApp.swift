24a67ddf7841e1accda643d1c7d9bfe9d2367950
Rexford Machu <rexfordmachu@gmail.com>
2021-11-16 06:12:34 +0000

    Initial Commit

---
+++ Reminder-Clone/Reminder_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reminder_CloneApp.swift
+//  Reminder-Clone
+//
+//  Created by Rexford Machu on 11/16/21.
+//
+
+import SwiftUI
+
+@main
+struct Reminder_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
